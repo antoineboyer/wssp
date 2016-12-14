a06a54905e7fe7e3383f5ad40d54672fdf6e0bb0
Antoine <antoineboyer31@gmail.com>
2016-12-14 21:00:39 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'PHP works fine             ';
+echo 'test';
+?>
---
+++ sprint1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="uft-8" />
+        <title>"Sprint 1</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>TIME (UTC)</th>
+                <th>MAGNITUDE (ML)</th>
+                <th>DEPTH (KM)</th>
+                <th>LAT/LONG (DEGREE)</th>
+                <th>REGION</th>
+                <th>M/A</th>
+            </tr>
+            <tr>
+                <td>2016-03-06 00:57:04</td>
+                <td>3.3</td>
+                <td>2</td>
+                <td>43.18/41.6043</td>
+                <td>City Tkvarcheli - North - 34km. Village Marjvena ptishi - 7km.</td>
+                <td>M</td>
+            </tr>
+            <tr>
+                <td>2016-12-11 02:56:48</td>
+                <td>3.2</td>
+                <td>20  </td>
+                <td>42.0715/45.9923</td>
+                <td>Russia. Dagestan. From Georgia border - 3km.</td>
+                <td>M</td>
+            </tr>
+        </table>
+    </body>
+</html>
